a70a668705cdfe0335e8347a6ff3db60cbd9615b
wind-surfing <echoinbyte@gmail.com>
2025-10-31 12:34:19 +0545

    feat: add InputField component with customizable props and styles, and implement NoPage component for 404 handling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,19 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./page/Layout";
 import Home from "./page/Home";
+import NoPage from "./components/shared/NoPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          {/* <Route path="login" element={<Login />} /> */}
-          {/* <Route path="shop" element={<Shop />} /> */}
-          {/* <Route path="*" element={<NoPage />} /> */}
-        </Route>
+        <Route index path="/" element={<Home />} />
+        {/* <Route path="login" element={<Login />} /> */}
+        {/* <Route path="shop" element={<Shop />} /> */}
+        <Route path="*" element={<NoPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
